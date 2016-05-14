393f3fad03fd853af3598aa0b207b8118a89d7e6
edcorners <esquinas.developer@gmail.com>
2016-05-14 19:56:21 -0400

    chore: Deleted old library folders

--- nojokes/src/main/java/com/udacity/gradle/builditbigger/java/NoJokes.java
+++
@@ -1,7 +0,0 @@
-package com.udacity.gradle.builditbigger.java;
-
-public class NoJokes {
-    public String tellAJoke(){
-        return "Error, keyboard not found. Press F1 to continue.";
-    }
-}
