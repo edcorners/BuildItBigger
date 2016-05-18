834998e505f96070add7942549d6f44b14442f94
edcorners <esquinas.developer@gmail.com>
2016-05-17 21:31:09 -0400

    chore: Endpoint test class created

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointTest.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.builditbigger;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Edison on 5/17/2016.
+ */
+public class EndpointTest extends AndroidTestCase {
+}
