9f517ac1667948cea0ea5f9a1882ee5ce952963d
edcorners <esquinas.developer@gmail.com>
2016-05-18 21:14:25 -0400

    chore: Created paid and free folders. Added flavors

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointTest.java
@@ -1,16 +1,12 @@
 package com.udacity.gradle.builditbigger;
 
-import android.app.Application;
 import android.content.Context;
 import android.test.AndroidTestCase;
-import android.test.ApplicationTestCase;
 
 import org.mockito.Mock;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Edison on 5/17/2016.
